7bf0919175b8e1a6df738c54ab523b7a6f9fe83b
Hanzei <hanzei@mailbox.org>
2018-10-04 13:34:57 +0200

    Add more code commments

--- server/utils.go
+++ server/utils.go
@@ -7,14 +7,22 @@ import (
 
 func ParseInput(input string, trigger string) (string, []string, []string) {
 	settings := []string{}
-	in := strings.TrimLeft(strings.TrimSpace(strings.TrimPrefix(input, fmt.Sprintf("/%s", trigger))), `"`)
 
+	// Remove Trigger prefix and spaces
+	in := strings.TrimSpace(strings.TrimPrefix(input, fmt.Sprintf("/%s", trigger)))
+	// Remove first "
+	in = strings.TrimLeft(in, `"`)
+
+	// Split between options
 	split := strings.Split(in, `" "`)
 	lastIndex := len(split) - 1
+
+	// Everything behind the last " are  Settings
 	l := strings.Split(split[lastIndex], string('"'))
 	split[lastIndex] = l[0]
 	if len(l) == 2 && l[1] != "" {
 		ops := strings.TrimPrefix(strings.TrimSpace(l[1]), "--")
+		// Split beween Settings
 		opsList := strings.Split(ops, "--")
 		for i := 0; i < len(opsList); i++ {
 			s := strings.TrimSpace(opsList[i])
@@ -22,6 +30,7 @@ func ParseInput(input string, trigger string) (string, []string, []string) {
 		}
 	}
 
+	// Unescape " in question and options
 	question := strings.Replace(split[0], `\"`, `"`, -1)
 	options := split[1:]
 	for i := 0; i < len(options); i++ {
