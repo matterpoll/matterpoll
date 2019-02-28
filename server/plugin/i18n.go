f42d2470d9edd431424a396183d24978dc9867f5
Hanzei <hanzei@mailbox.org>
2019-02-28 15:05:39 +0100

    Load all translation files in i18n

--- server/plugin/i18n.go
+++ server/plugin/i18n.go
@@ -2,6 +2,8 @@ package plugin
 
 import (
 	"encoding/json"
+	"io/ioutil"
+	"strings"
 
 	"github.com/matterpoll/matterpoll/server/utils"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
@@ -13,10 +15,24 @@ func initBundle() (*i18n.Bundle, error) {
 	bundle := &i18n.Bundle{DefaultLanguage: language.English}
 	bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
 
-	i18nPath := utils.GetPluginRootPath() + "/i18n"
-	_, err := bundle.LoadMessageFile(i18nPath + "/active.de.json")
+	i18nDir := utils.GetPluginRootPath() + "/i18n"
+	files, err := ioutil.ReadDir(i18nDir)
 	if err != nil {
-		return nil, errors.Wrapf(err, "failed to load message file %s", "de.json")
+		return nil, errors.Wrap(err, "failed to open i18n directory")
+	}
+
+	for _, file := range files {
+		if !strings.HasPrefix(file.Name(), "active.") {
+			continue
+		}
+
+		if file.Name() == "active.en.json" {
+			continue
+		}
+		_, err = bundle.LoadMessageFile(i18nDir + "/" + file.Name())
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to load message file %s", file.Name())
+		}
 	}
 
 	return bundle, nil
