0bab4ae8fdd5327bf33cb4956d5302a0e0758902
Hanzei <16541325+hanzei@users.noreply.github.com>
2019-08-11 15:37:21 +0200

    Various code fixes  (#183)
    * Close bodies
    
    * Various code fixes

--- server/plugin/api_test.go
+++ server/plugin/api_test.go
@@ -54,6 +54,7 @@ func TestServeHTTP(t *testing.T) {
 
 			result := w.Result()
 			require.NotNil(t, result)
+			defer result.Body.Close()
 
 			bodyBytes, err := ioutil.ReadAll(result.Body)
 			require.Nil(t, err)
@@ -105,6 +106,7 @@ func TestServeFile(t *testing.T) {
 
 			result := w.Result()
 			require.NotNil(t, result)
+			defer result.Body.Close()
 
 			bodyBytes, err := ioutil.ReadAll(result.Body)
 			require.Nil(t, err)
@@ -132,7 +134,10 @@ func TestHandleVote(t *testing.T) {
 		w := httptest.NewRecorder()
 		r := httptest.NewRequest(http.MethodPost, fmt.Sprintf("/api/v1/polls/%s/vote/0", testutils.GetPollID()), bytes.NewReader(request.ToJson()))
 		p.ServeHTTP(nil, w, r)
+
 		result := w.Result()
+		require.NotNil(t, result)
+		defer result.Body.Close()
 
 		assert.Equal(t, http.StatusUnauthorized, result.StatusCode)
 	})
@@ -282,6 +287,7 @@ func TestHandleVote(t *testing.T) {
 
 			result := w.Result()
 			require.NotNil(t, result)
+			defer result.Body.Close()
 			response := model.PostActionIntegrationResponseFromJson(result.Body)
 
 			assert.Equal(test.ExpectedStatusCode, result.StatusCode)
@@ -313,6 +319,7 @@ func TestHandleAddOption(t *testing.T) {
 		r := httptest.NewRequest(http.MethodPost, fmt.Sprintf("/api/v1/polls/%s/option/add", testutils.GetPollID()), bytes.NewReader(request.ToJson()))
 		p.ServeHTTP(nil, w, r)
 		result := w.Result()
+		defer result.Body.Close()
 
 		assert.Equal(t, http.StatusUnauthorized, result.StatusCode)
 	})
@@ -383,6 +390,7 @@ func TestHandleAddOption(t *testing.T) {
 
 			result := w.Result()
 			require.NotNil(t, result)
+			defer result.Body.Close()
 			response := model.SubmitDialogResponseFromJson(result.Body)
 
 			assert.Equal(test.ExpectedStatusCode, result.StatusCode)
@@ -411,7 +419,10 @@ func TestHandleAddOptionDialogRequest(t *testing.T) {
 		w := httptest.NewRecorder()
 		r := httptest.NewRequest(http.MethodPost, fmt.Sprintf("/api/v1/polls/%s/option/add/request", testutils.GetPollID()), bytes.NewReader(request.ToJson()))
 		p.ServeHTTP(nil, w, r)
+
 		result := w.Result()
+		require.NotNil(t, result)
+		defer result.Body.Close()
 
 		assert.Equal(t, http.StatusUnauthorized, result.StatusCode)
 	})
@@ -495,6 +506,7 @@ func TestHandleAddOptionDialogRequest(t *testing.T) {
 
 			result := w.Result()
 			require.NotNil(t, result)
+			defer result.Body.Close()
 			response := model.PostActionIntegrationResponseFromJson(result.Body)
 
 			assert.Equal(test.ExpectedStatusCode, result.StatusCode)
@@ -519,7 +531,10 @@ func TestHandleEndPoll(t *testing.T) {
 		w := httptest.NewRecorder()
 		r := httptest.NewRequest(http.MethodPost, fmt.Sprintf("/api/v1/polls/%s/end", testutils.GetPollID()), bytes.NewReader(request.ToJson()))
 		p.ServeHTTP(nil, w, r)
+
 		result := w.Result()
+		require.NotNil(t, result)
+		defer result.Body.Close()
 
 		assert.Equal(t, http.StatusUnauthorized, result.StatusCode)
 	})
@@ -701,6 +716,7 @@ func TestHandleEndPoll(t *testing.T) {
 
 			result := w.Result()
 			require.NotNil(t, result)
+			defer result.Body.Close()
 			response := model.PostActionIntegrationResponseFromJson(result.Body)
 
 			assert.Equal(test.ExpectedStatusCode, result.StatusCode)
@@ -791,7 +807,10 @@ func TestHandleDeletePoll(t *testing.T) {
 		w := httptest.NewRecorder()
 		r := httptest.NewRequest(http.MethodPost, fmt.Sprintf("/api/v1/polls/%s/delete", testutils.GetPollID()), bytes.NewReader(request.ToJson()))
 		p.ServeHTTP(nil, w, r)
+
 		result := w.Result()
+		require.NotNil(t, result)
+		defer result.Body.Close()
 
 		assert.Equal(t, http.StatusUnauthorized, result.StatusCode)
 	})
@@ -931,6 +950,7 @@ func TestHandleDeletePoll(t *testing.T) {
 
 			result := w.Result()
 			require.NotNil(t, result)
+			defer result.Body.Close()
 			response := model.PostActionIntegrationResponseFromJson(result.Body)
 
 			assert.Equal(test.ExpectedStatusCode, result.StatusCode)
