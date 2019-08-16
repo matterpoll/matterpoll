c81175766cd735ef287108375139554119402114
kaakaa <stooner.hoe@gmail.com>
2019-08-17 01:19:24 +0900

    Fix for staying consistent with mattermost-plugin-starter-template

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -19,10 +19,26 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
                         plugins: [
                             '@babel/plugin-proposal-class-properties',
                             '@babel/plugin-syntax-dynamic-import',
+                        ],
+                        presets: [
+                            ['@babel/preset-env', {
+                                targets: {
+                                    chrome: 66,
+                                    firefox: 60,
+                                    edge: 42,
+                                    safari: 12,
+                                },
+                                modules: false,
+                                debug: false,
+                                useBuiltIns: 'usage',
+                                shippedProposals: true,
+                            }],
+                            ['@babel/preset-react', {
+                                useBuiltIns: true,
+                            }],
                         ],
                     },
                 },
@@ -33,6 +49,8 @@ module.exports = {
         react: 'React',
         redux: 'Redux',
         'react-redux': 'ReactRedux',
+        'prop-types': 'PropTypes',
+        'react-bootstrap': 'ReactBootstrap',
     },
     output: {
         path: path.join(__dirname, '/dist'),
