bf0845c6e39d613fd35440c765d2cfa2085681b6
kaakaa <stooner.hoe@gmail.com>
2019-08-17 11:47:54 +0900

    Fix problems from check-style

--- webapp/src/client/client.js
+++ webapp/src/client/client.js
@@ -15,69 +15,53 @@ export default class Client {
         headers['X-Requested-With'] = 'XMLHttpRequest';
         headers['X-Timezone-Offset'] = new Date().getTimezoneOffset();
 
-        try {
-            const response = await request.
-                get(url).
-                set(headers).
-                accept('application/json');
+        const response = await request.
+            get(url).
+            set(headers).
+            accept('application/json');
 
-            return response.body;
-        } catch (err) {
-            throw err;
-        }
+        return response.body;
     }
 
     doPost = async (url, body, headers = {}) => {
         headers['X-Requested-With'] = 'XMLHttpRequest';
         headers['X-Timezone-Offset'] = new Date().getTimezoneOffset();
 
-        try {
-            const response = await request.
-                post(url).
-                send(body).
-                set(headers).
-                type('application/json').
-                accept('application/json');
+        const response = await request.
+            post(url).
+            send(body).
+            set(headers).
+            type('application/json').
+            accept('application/json');
 
-            return response.body;
-        } catch (err) {
-            throw err;
-        }
+        return response.body;
     }
 
     doDelete = async (url, body, headers = {}) => {
         headers['X-Requested-With'] = 'XMLHttpRequest';
         headers['X-Timezone-Offset'] = new Date().getTimezoneOffset();
 
-        try {
-            const response = await request.
-                delete(url).
-                send(body).
-                set(headers).
-                type('application/json').
-                accept('application/json');
+        const response = await request.
+            delete(url).
+            send(body).
+            set(headers).
+            type('application/json').
+            accept('application/json');
 
-            return response.body;
-        } catch (err) {
-            throw err;
-        }
+        return response.body;
     }
 
     doPut = async (url, body, headers = {}) => {
         headers['X-Requested-With'] = 'XMLHttpRequest';
         headers['X-Timezone-Offset'] = new Date().getTimezoneOffset();
 
-        try {
-            const response = await request.
-                put(url).
-                send(body).
-                set(headers).
-                type('application/json').
-                accept('application/json');
+        const response = await request.
+            put(url).
+            send(body).
+            set(headers).
+            type('application/json').
+            accept('application/json');
 
-            return response.body;
-        } catch (err) {
-            throw err;
-        }
+        return response.body;
     }
 }
