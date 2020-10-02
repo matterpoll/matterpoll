ca8e8f0e0aaaf8959c8bb9231768c3fb517b2443
Ben Schumacher <ben.schumacher@mattermost.com>
2020-10-02 17:50:51 +0200

    Use index.tsx

---
+++ webapp/src/index.tsx
@@ -0,0 +1,10 @@
+import MatterPollPlugin from './plugin';
+import manifest from './manifest';
+
+declare global {
+    interface Window {
+        registerPlugin(id: string, plugin: MatterPollPlugin): void
+    }
+}
+
+window.registerPlugin(manifest.id, new MatterPollPlugin());
--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -36,7 +36,7 @@ if (NPM_TARGET === 'build:watch' || NPM_TARGET === 'debug:watch') {
 
 module.exports = {
     entry: [
-        './src/index.js',
+        './src/index.tsx',
     ],
     resolve: {
         modules: [
