caa6d1a35812af0109124223c30c2066a42b85da
Yusuke Nemoto <kaakaa@users.noreply.github.com>
2020-10-23 21:33:50 +0900

    Update server/poll/metadata.go
    Co-authored-by: Ben Schumacher <ben.schumacher@mattermost.com>

--- server/poll/metadata.go
+++ server/poll/metadata.go
@@ -5,7 +5,7 @@ type Metadata struct {
 	VotedAnswers           []string `json:"voted_answers"` // VotedAnswers is list of answer that the user with "UserID" have voted for the poll with "PollID"
 	PollID                 string   `json:"poll_id"`
 	UserID                 string   `json:"user_id"`
-	CanManagePoll          bool     `json:"can_manage_poll"` // CanManagePoll will be true if the user with "UserID" has admin permission for the poll with "PollID", otherwise false.
+	CanManagePoll          bool     `json:"can_manage_poll"` // CanManagePoll will be true if the user with "UserID" can manage the poll with "PollID", otherwise false.
 	SettingPublicAddOption bool     `json:"setting_public_add_option"`
 }
 
