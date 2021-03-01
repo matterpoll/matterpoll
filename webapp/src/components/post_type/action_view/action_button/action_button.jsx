72dbc008bea028613d6eceb6f50afccadc27a4b0
kaakaa <stooner.hoe@gmail.com>
2021-03-01 21:35:53 +0900

    set button color when experimental ui is enabled

--- webapp/src/components/post_type/action_view/action_button/action_button.jsx
+++ webapp/src/components/post_type/action_view/action_button/action_button.jsx
@@ -1,18 +1,36 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import {changeOpacity} from 'mattermost-redux/utils/theme_utils';
+import invert from 'invert-color';
+
 const PostUtils = window.PostUtils;
-const {Button, ButtonToolbar} = window.ReactBootstrap;
 
 export default class ActionButton extends React.PureComponent {
     static propTypes = {
         action: PropTypes.object.isRequired,
         postId: PropTypes.string.isRequired,
+        theme: PropTypes.object.isRequired,
         hasVoted: PropTypes.bool,
 
         actions: PropTypes.shape({
             voteAnswer: PropTypes.func.isRequired,
         }).isRequired,
+    }
+
+    getStatusColors = (theme) => {
+        return {
+            good: '#00c100',
+            warning: '#dede01',
+            danger: theme.errorTextColor,
+            default: theme.centerChannelColor,
+            primary: theme.buttonBg,
+            success: theme.onlineIndicator,
+        };
+    }
+
+    invertColor = (color) => {
+        return color.match('^#(?:[0-9a-fA-F]{3}){1,2}$') ? invert(color) : color;
     }
 
     handleAction = (e) => {
@@ -26,7 +44,7 @@ export default class ActionButton extends React.PureComponent {
     };
 
     render() {
-        const {action} = this.props;
+        const {action, theme} = this.props;
 
         const htmlFormattedText = PostUtils.formatText(action.name, {
             mentionHighlight: false,
@@ -34,25 +52,43 @@ export default class ActionButton extends React.PureComponent {
             autoLinkedUrlSchemes: [],
         });
         const message = PostUtils.messageHtmlToComponent(htmlFormattedText, false, {emoji: true});
-        const bsStyle = this.props.hasVoted ? 'primary' : 'default';
+
+        let customButtonStyle;
+        if (action.style) {
+            const STATUS_COLORS = this.getStatusColors(theme);
+            const hexColor =
+                STATUS_COLORS[action.style] ||
+                theme[action.style] ||
+                (action.style.match('^#(?:[0-9a-fA-F]{3}){1,2}$') && action.style);
+            if (hexColor) {
+                if (this.props.hasVoted) {
+                    customButtonStyle = {
+                        borderColor: changeOpacity(this.invertColor(hexColor), 0.25),
+                        backgroundColor: changeOpacity(this.invertColor(theme.centerChannelBg), 0.75),
+                        color: this.invertColor(hexColor),
+                        borderWidth: 2,
+                    };
+                } else {
+                    customButtonStyle = {
+                        borderColor: changeOpacity(hexColor, 0.25),
+                        backgroundColor: theme.centerChannelBg,
+                        color: hexColor,
+                        borderWidth: 2,
+                    };
+                }
+            }
+        }
 
         return (
-            <ButtonToolbar
-                style={style.buttonToolbar}
+            <button
+                data-action-id={action.id}
+                data-action-cookie={action.cookie}
+                key={action.id}
+                onClick={this.handleAction}
+                style={customButtonStyle}
             >
-                <Button
-                    data-action-id={action.id}
-                    key={action.id}
-                    onClick={this.handleAction}
-                    bsStyle={bsStyle}
-                >
-                    {message}
-                </Button>
-            </ButtonToolbar>
+                {message}
+            </button>
         );
     }
 }
-
-const style = {
-    buttonToolbar: {marginLeft: 0},
-};
