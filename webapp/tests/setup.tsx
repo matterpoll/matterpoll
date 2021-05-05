866ecc31261354036d3cb645ea70061ad75e263f
kaakaa <stooner.hoe@gmail.com>
2021-05-05 18:22:22 +0900

    build: make sync

--- webapp/tests/setup.tsx
+++ webapp/tests/setup.tsx
@@ -1,11 +1,10 @@
-import '@babel/polyfill';
-import Adapter from 'enzyme-adapter-react-16';
-import {configure} from 'enzyme';
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
 
-configure({adapter: new Adapter()});
+import 'mattermost-webapp/tests/setup';
+import '@babel/polyfill';
 
 global.ReactBootstrap = require('react-bootstrap');
-
 global.PostUtils = {
     formatText: jest.fn().mockImplementation((t) => 'mockFormatText(' + t + ')'),
     messageHtmlToComponent: jest.fn().mockImplementation((t) => 'mockMessageHtmlToComponent(' + t + ')'),
