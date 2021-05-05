36254a1cb273069334cfb5b3a5e28c68d3d81feb
kaakaa <stooner.hoe@gmail.com>
2021-05-05 14:53:29 +0900

    test: fix tests

--- webapp/src/components/post_type/action_view/action_button/action_button.test.jsx
+++ webapp/src/components/post_type/action_view/action_button/action_button.test.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 
+import {Constants} from 'mattermost-webapp/utils/constants';
+
 import ActionButton from 'components/post_type/action_view/action_button/action_button';
 
 describe('components/action_button/ActionButton', () => {
@@ -10,16 +12,17 @@ describe('components/action_button/ActionButton', () => {
             id: 'action_id1',
             name: 'action_name',
         },
+        theme: Constants.THEMES.default,
         hasVoted: false,
         actions: {
             voteAnswer: jest.fn(),
         },
     };
-    test('should match snapshot', () => {
+    test('without style should match snapshot', () => {
         const wrapper = shallow(<ActionButton {...baseProps}/>);
         expect(wrapper).toMatchSnapshot();
     });
-    test('should match snapshot with hasVoted', () => {
+    test('without style with hasVoted should match snapshot', () => {
         const newProps = {
             ...baseProps,
             hasVoted: true,
