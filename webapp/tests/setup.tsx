bf3480a08edab6383d12dccc0e595da581dace00
kaakaa <stooner.hoe@gmail.com>
2021-10-01 12:22:19 +0900

    remove header comments

--- webapp/tests/setup.tsx
+++ webapp/tests/setup.tsx
@@ -1,6 +1,3 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
-
 import 'mattermost-webapp/tests/setup';
 import '@babel/polyfill';
 import {jest} from '@jest/globals';
