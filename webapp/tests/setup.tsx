86745db849ed7153d541ae939bdd10bb8229b840
mickmister <6913320+mickmister@users.noreply.github.com>
2024-01-11 13:15:40 -0500

    remove mattermost-webapp dep

--- webapp/tests/setup.tsx
+++ webapp/tests/setup.tsx
@@ -1,6 +1,10 @@
-import 'mattermost-webapp/tests/setup';
 import '@babel/polyfill';
 import {jest} from '@jest/globals';
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({adapter: new Adapter()});
 
 // @ts-ignore
 global.ReactBootstrap = require('react-bootstrap');
