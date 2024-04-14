8395d9f7e576b64fe22003af92a7d1ee8beeabc5
Anna Ostrovskaya <admin@annaos.dev>
2024-04-14 16:51:03 +0200

    [GH-248] add help test to custom admin setting

--- webapp/src/components/admin_settings/default_settings.jsx
+++ webapp/src/components/admin_settings/default_settings.jsx
@@ -66,6 +66,9 @@ export default class DefaultSettings extends React.Component {
                     value={this.settings.publicAddOption}
                     onChange={this.handleChange}
                 />
+                <div className={'help-text'}>
+                    <span>{'Settings will be pre-selected in \'Create Poll\' dialog. Settings will not be applied to `/poll` command.'}</span>
+                </div>
             </div>
         );
     }
